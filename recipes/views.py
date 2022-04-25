ea7388226c1190e6fa7b6bd3c11cc87894f78c00
Idevaldo <inetomec@yahoo.com.br>
2022-04-25 09:36:06 -0300

    feat: :sparkles: Testing 404 not found when there is no recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
@@ -13,23 +11,23 @@ def home(request):
         'recipes': recipes,
     })
 
+
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
-    if not recipes:
-        raise Http404('Not found 🥲')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
+
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
